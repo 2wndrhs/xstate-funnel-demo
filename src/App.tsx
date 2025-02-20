72d909ab15ab57291d1e3daa8eb4833a8cadba76
2wndrhs <2wndrhs@gmail.com>
2025-02-20 19:12:05 +0900

    feat: 라우팅 적용

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-  return <h1 className="text-xl font-extrabold">Hello Vite React</h1>;
-};
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
+import Layout from './layouts/Layout.tsx';
+import AdmissionYearInput from './pages/AdmissionYearInput.tsx';
+import DepartmentInput from './pages/DepartmentInput.tsx';
+import GradeInput from './pages/GradeInput.tsx';
+import TimetablePage from './pages/TimetablePage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<DepartmentInput />} />
+          <Route path="admissionYear" element={<AdmissionYearInput />} />
+          <Route path="grade" element={<GradeInput />} />
+          <Route path="timetable" element={<TimetablePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
