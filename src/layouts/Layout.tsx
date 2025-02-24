6405376c838c1062ac1e0736a2f39ed20f616409
2wndrhs <2wndrhs@gmail.com>
2025-02-24 19:27:58 +0900

    feat: 동적 진행률 표시를 위한 AppBar 컴포넌트 수정

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,12 +2,20 @@ import { motion } from 'motion/react';
 import { Outlet, useLocation } from 'react-router';
 import AppBar from '../components/AppBar';
 
+const progressMap: Record<string, number> = {
+  '/': 25,
+  '/admissionYear': 50,
+  '/grade': 75,
+  '/timetable': 100,
+};
+
 const Layout = () => {
   const location = useLocation();
+  const progress = progressMap[location.pathname];
 
   return (
     <div className="font-pretendard flex min-h-dvh flex-col py-6">
-      <AppBar progress={50} />
+      <AppBar progress={progress} />
       <motion.div
         key={location.pathname}
         initial={{ opacity: 0, y: 20 }}
